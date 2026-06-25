afbca5b553609f8b123aa02e70350a9b73cefe5e
BST <107313913+alvin000009238@users.noreply.github.com>
2026-06-25 19:18:00 +0800

    feat: updated branding assets to app layout

--- app/app.vue
+++ app/app.vue
@@ -35,11 +35,26 @@ useHead(() => ({
     {
       rel: 'icon',
       type: 'image/png',
-      href: '/icon-192.png',
+      href: '/favicon-96x96.png',
+      sizes: '96x96',
+    },
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg',
+    },
+    {
+      rel: 'shortcut icon',
+      href: '/favicon.ico',
     },
     {
       rel: 'apple-touch-icon',
+      sizes: '180x180',
       href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
     },
   ],
 }))
